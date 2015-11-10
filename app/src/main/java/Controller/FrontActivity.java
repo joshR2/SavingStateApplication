049a2ccf29f18063d82c4559d2eb07a690150275
joshR2 <josh2cool365@aol.com>
2015-11-10 09:40:49 -0700

    SavingState App
    not sure what i’ve done… i’ll open it later…

---
+++ app/src/main/java/Controller/FrontActivity.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by jrob2049 on 10/14/15.
+ */
+public class FrontActivity {
+}
---
+++ app/src/main/java/Model/AndroidSaveState.java
@@ -0,0 +1,40 @@
+package Model;
+
+import android.app.Application;
+
+/**
+ * Created by jrob2049 on 10/14/15.
+ */
+public class AndroidSaveState extends Application
+{
+    private String userName;
+    private boolean isTired;
+    private int age;
+
+    public AndroidSaveState()
+    {
+        this.userName = "default";
+        this.isTired = false;
+        this.age = -0;
+    }
+    
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public boolean getisTired()
+    {
+        return isTired;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName
+    }
+}
